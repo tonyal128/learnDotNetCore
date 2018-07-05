0be803224303f18f8439e0d01cffdb8e2e1d1778
Anthony <pepsispep@live.com>
2018-07-05 17:41:42 -0500

    Add about controller

---
+++ learnDotNetCore/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.Controllers
+{
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "5255255252";
+        }
+
+        public string Address()
+        {
+            return "45th st";
+        }
+    }
+}
