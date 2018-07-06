7fb24dbbbb1a69a5803ae9486f57d4d2a7bc0bc5
Anthony <pepsispep@live.com>
2018-07-05 19:56:29 -0500

    Creating interface for restaurant data

---
+++ learnDotNetCore/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using learnDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.Services
+{
+    interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
