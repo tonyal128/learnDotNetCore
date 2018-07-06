e5181be735ba95828f9a154070039c3a155efb1b
Anthony <pepsispep@live.com>
2018-07-06 09:14:20 -0500

    Creating view model to include all information the view needs

---
+++ learnDotNetCore/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using learnDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public string CurrentMessage { get; set; }
+    }
+}
