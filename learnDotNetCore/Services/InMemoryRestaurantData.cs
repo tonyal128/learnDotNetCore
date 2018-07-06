9071e913557781c539d1764254227a0389699cef
Anthony <pepsispep@live.com>
2018-07-05 20:27:12 -0500

    created implementation of IRestaurantData that uses hard coded values for testing purposes

---
+++ learnDotNetCore/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using learnDotNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id = 1, Name = "Pizza Place"},
+                new Restaurant{Id = 2, Name = "Huhot"},
+                new Restaurant{Id = 3, Name = "Pub"},
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        List<Restaurant> _restaurants;
+    }
+}
