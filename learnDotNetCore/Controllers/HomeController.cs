fe14a65a2f3c1d38d0692a356f790235f25bdcc1
Anthony <pepsispep@live.com>
2018-07-05 19:41:08 -0500

    Returning home view

--- learnDotNetCore/Controllers/HomeController.cs
+++ learnDotNetCore/Controllers/HomeController.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿using learnDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace learnDotNetCore.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from Home Controller";
+            var model = new Restaurant { Id = 1, Name = "Pizza Place" };
+            return View(model);
         }
     }
 }
