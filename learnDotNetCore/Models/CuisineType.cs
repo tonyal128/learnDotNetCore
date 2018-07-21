2002615d14fcaa37192b3a820a6a98fbc3b25993
Anthony <pepsispep@live.com>
2018-07-21 03:00:10 -0500

    Creating enum to hold cuisine types

---
+++ learnDotNetCore/Models/CuisineType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        American,
+        Chinese,
+        Mongolian,
+        German
+    }
+}
