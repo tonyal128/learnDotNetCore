75b8960ee623fc9045f36a90f9af7bbaf80e8df7
Anthony <pepsispep@live.com>
2018-07-24 21:10:55 -0500

    Creating input model for adding a new restaurant

---
+++ learnDotNetCore/ViewModels/RestaurantEditModel.cs
@@ -0,0 +1,10 @@
+﻿using learnDotNetCore.Models;
+
+namespace learnDotNetCore.ViewModels
+{
+    public class ResaurantEditModel
+    {
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
