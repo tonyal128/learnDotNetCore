e87c1a92e7a4f4b2ee04a9822a928ae1903de438
Anthony <pepsispep@live.com>
2018-07-24 21:13:15 -0500

    Implementing create method to create new restaurants

--- learnDotNetCore/Controllers/HomeController.cs
+++ learnDotNetCore/Controllers/HomeController.cs
@@ -39,9 +39,22 @@ namespace learnDotNetCore.Controllers
             return View(model);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ResaurantEditModel model)
+        {
+            var newRestaurant = new Restaurant();
+            newRestaurant.Name = model.Name;
+            newRestaurant.Cuisine = model.Cuisine;
+
+            newRestaurant = _restaurantData.Add(newRestaurant);
+
+            return RedirectToAction(nameof(Details),new { id = newRestaurant.Id });
         }
     }
 }
