315759aef5ac4d30a746a4b4ad5c038df33aa097
Anthony <pepsispep@live.com>
2018-07-27 12:42:16 -0500

    adding entity models

---
+++ learnDotNetCore/Data/LearnDotNetCoreDBContext.cs
@@ -0,0 +1,15 @@
+﻿using learnDotNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace learnDotNetCore.Data
+{
+    public class LearnDotNetCoreDBContext : DbContext
+    {
+        public LearnDotNetCoreDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
