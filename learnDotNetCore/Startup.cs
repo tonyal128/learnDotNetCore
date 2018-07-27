82e6a31f2e4fc348939d62a4470cc6b76088a84c
Anthony <anthony.r.larson@gmail.com>
2018-07-27 18:58:34 -0500

    Adding configuration to connect to sql server

--- learnDotNetCore/Startup.cs
+++ learnDotNetCore/Startup.cs
@@ -1,20 +1,30 @@
-﻿using learnDotNetCore.Services;
+﻿using learnDotNetCore.Data;
+using learnDotNetCore.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace learnDotNetCore
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            services.AddDbContext<LearnDotNetCoreDBContext>(options => options.UseSqlServer(_configuration.GetConnectionString("LearnDotNetCore")));
             services.AddMvc();
         }
 
