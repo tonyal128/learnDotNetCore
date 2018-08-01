467368b8f67f5b21f09937430b595c7c8f1342c0
Anthony <anthony.r.larson@gmail.com>
2018-08-01 17:21:53 -0500

    commenting out this file

--- learnDotNetCore/Services/InMemoryRestaurantData.cs
+++ learnDotNetCore/Services/InMemoryRestaurantData.cs
@@ -4,36 +4,36 @@ using System.Linq;
 
 namespace learnDotNetCore.Services
 {
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        public InMemoryRestaurantData()
-        {
-            _restaurants = new List<Restaurant>
-            {
-                new Restaurant{Id = 1, Name = "Pizza Place"},
-                new Restaurant{Id = 2, Name = "Huhot"},
-                new Restaurant{Id = 3, Name = "Pub"},
-            };
-        }
+    //public class InMemoryRestaurantData : IRestaurantData
+    //{
+    //    public InMemoryRestaurantData()
+    //    {
+    //        _restaurants = new List<Restaurant>
+    //        {
+    //            new Restaurant{Id = 1, Name = "Pizza Place"},
+    //            new Restaurant{Id = 2, Name = "Huhot"},
+    //            new Restaurant{Id = 3, Name = "Pub"},
+    //        };
+    //    }
 
-        public IEnumerable<Restaurant> GetAll()
-        {
-            return _restaurants.OrderBy(r => r.Name);
-        }
+    //    public IEnumerable<Restaurant> GetAll()
+    //    {
+    //        return _restaurants.OrderBy(r => r.Name);
+    //    }
 
-        public Restaurant Get(int id)
-        {
-            return _restaurants.FirstOrDefault(r => r.Id == id);
-        }
+    //    public Restaurant Get(int id)
+    //    {
+    //        return _restaurants.FirstOrDefault(r => r.Id == id);
+    //    }
 
-        public Restaurant Add(Restaurant restaurant)
-        {
-            restaurant.Id = _restaurants.Max(r => r.Id) + 1;
-            _restaurants.Add(restaurant);
+    //    public Restaurant Add(Restaurant restaurant)
+    //    {
+    //        restaurant.Id = _restaurants.Max(r => r.Id) + 1;
+    //        _restaurants.Add(restaurant);
 
-            return restaurant;
-        }
+    //        return restaurant;
+    //    }
 
-        List<Restaurant> _restaurants;
-    }
+    //    List<Restaurant> _restaurants;
+    //}
 }
