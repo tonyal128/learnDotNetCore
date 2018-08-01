7f2be0c4b16601331919c5a9bcd1780dd760535f
Anthony <anthony.r.larson@gmail.com>
2018-08-01 18:56:58 -0500

    Creating view component

---
+++ learnDotNetCore/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using learnDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnDotNetCore.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+            return View("Default",model);
+        }
+    }
+}
